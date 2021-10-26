9a20b3302e7e950c90a10a5fd82a6ca790e62b8e
JasmineJerryAloor <aloorkokken@gmail.com>
2021-10-27 00:11:51 +0530

    corrected indentation

--- gym.py
+++ gym.py
@@ -10,68 +10,68 @@ from net import *
 
 class Gym():
 
-    def __init__(self,datapath,args):
-        
-        self.datapath = datapath
-        self.args = args
-        self.load_trajair()
-        self.load_action()
-	self.traj_lib, self.index_lib = populate_traj_lib()
+	def __init__(self,datapath,args):
+		
+		self.datapath = datapath
+		self.args = args
+		self.load_trajair()
+		self.load_action()
+		self.traj_lib, self.index_lib = populate_traj_lib()
 
-    def load_action(self):
+	def load_action(self):
 
-	# to load model here
-	model = TCN(input_channels, output_size, channel_sizes, kernel_size=kernel_size, dropout=dropout)
-	model.to(device)
+		# to load model here
+		model = TCN(input_channels, output_size, channel_sizes, kernel_size=kernel_size, dropout=dropout)
+		model.to(device)
 
-	checkpoint = torch.load('goalGAIL2_60.pt',map_location=torch.device('cpu'))
-	model.load_state_dict(checkpoint['model_state_dict'], strict=False)
+		checkpoint = torch.load('goalGAIL2_60.pt',map_location=torch.device('cpu'))
+		model.load_state_dict(checkpoint['model_state_dict'], strict=False)
 
-        softmax_out, output, latent_space_, multi_head = model(torch.transpose(obs_traj_all,1,2),torch.transpose(pred_traj_all,1,2),goal_position,torch.transpose(context,1,2),sort=True)
+		softmax_out, output, latent_space_, multi_head = model(torch.transpose(obs_traj_all,1,2),torch.transpose(pred_traj_all,1,2),goal_position,torch.transpose(context,1,2),sort=True)
 
-	traj_lib_choice = torch.argmax(recon_x,dim=0) # dim = 0 if single traj in a batch
-	return traj_lib_choice
+		traj_lib_choice = torch.argmax(recon_x,dim=0) # dim = 0 if single traj in a batch
+		return traj_lib_choice
 
 
-    def load_trajair(self):
+	def load_trajair(self):
 
-        dataset_train = TrajectoryDataset(self.datapath + "test", obs_len=self.args.obs, pred_len=self.args.preds, step=self.args.preds_step, delim=self.args.delim)
-        self.loader_train = DataLoader(dataset_train,batch_size=1,num_workers=4,shuffle=True,collate_fn=seq_collate)
+		dataset_train = TrajectoryDataset(self.datapath + "test", obs_len=self.args.obs, pred_len=self.args.preds, step=self.args.preds_step, delim=self.args.delim)
+		self.loader_train = DataLoader(dataset_train,batch_size=1,num_workers=4,shuffle=True,collate_fn=seq_collate)
 
-    def get_random_start_position(self):
+	def get_random_start_position(self):
 
-        obs_traj , pred_traj, obs_traj_rel, pred_traj_rel, context, seq_start,goal_position,full_l2 = next(iter(self.loader_train)) 
-        return obs_traj
+		obs_traj , pred_traj, obs_traj_rel, pred_traj_rel, context, seq_start,goal_position,full_l2 = next(iter(self.loader_train)) 
+		return obs_traj
 
-    def get_random_goal_location(self,num_goals = 10):
-        
-        return  np.eye(num_goals)[np.random.choice(num_goals, 1)]
+	def get_random_goal_location(self,num_goals = 10):
+		
+		return  np.eye(num_goals)[np.random.choice(num_goals, 1)]
 
-    def getNextState(self):
+	def getNextState(self):
 
-	action_choice = load_action()
-	obs_traj = get_random_start_position()
+		action_choice = load_action()
+		obs_traj = get_random_start_position()
 
-	# rotate and translate action choice to end of previous executed traj
-	difference = obs_traj[-1,:,:]-obs_traj[-3,:,:]
-        difference = difference.cpu().numpy() # verify device
-        unit_vector_1 = difference[:,0:2] / np.linalg.norm(difference[:,0:2])
-        angle = np.arctan2(difference[:,1],difference[:,0])
-        r = R.from_euler('z',angle)
-        direction_matrx_rep = r.as_matrix()
+		# rotate and translate action choice to end of previous executed traj
+		difference = obs_traj[-1,:,:]-obs_traj[-3,:,:]
+		difference = difference.cpu().numpy() # verify device
+		unit_vector_1 = difference[:,0:2] / np.linalg.norm(difference[:,0:2])
+		angle = np.arctan2(difference[:,1],difference[:,0])
+		r = R.from_euler('z',angle)
+		direction_matrx_rep = r.as_matrix()
 
-	trajs = np.zeros([20,3])
-        for j in range(20):
-        	trajs[j,0:2] = torch.from_numpy(np.dot(direction_matrx_rep[0:2,0:2],self.traj_lib[action_choice,0:2,j].T))
-        for i in range(20):
-        	trajs[i,0]+=obs_traj[-1,0]
-        	trajs[i,1]+=obs_traj[-1,1]
-        	trajs[i,2]=self.traj_lib[traj_lib_choice,2,i]+obs_traj[-1,2]
+		trajs = np.zeros([20,3])
+		for j in range(20):
+			trajs[j,0:2] = torch.from_numpy(np.dot(direction_matrx_rep[0:2,0:2],self.traj_lib[action_choice,0:2,j].T))
+		for i in range(20):
+			trajs[i,0]+=obs_traj[-1,0]
+			trajs[i,1]+=obs_traj[-1,1]
+			trajs[i,2]=self.traj_lib[traj_lib_choice,2,i]+obs_traj[-1,2]
 
-	# trajs is a numpy array
-        return trajs
+		# trajs is a numpy array
+		return trajs
 
-    def getActionSize(self):
+	def getActionSize(self):
 
-	action_space_size = self.traj_lib.shape[0]
-        return action_space_size
+		action_space_size = self.traj_lib.shape[0]
+		return action_space_size
--- net.py
+++ net.py
@@ -3,77 +3,77 @@ from utils import TemporalConvNet
 
 
 class TCN(nn.Module):
-    def __init__(self, input_size, output_size, num_channels, kernel_size, dropout,context=True):
-        super(TCN, self).__init__()
-        self.tcn_encoder_x = TemporalConvNet(input_size, num_channels, kernel_size=kernel_size, dropout=dropout)
+	def __init__(self, input_size, output_size, num_channels, kernel_size, dropout,context=True):
+		super(TCN, self).__init__()
+		self.tcn_encoder_x = TemporalConvNet(input_size, num_channels, kernel_size=kernel_size, dropout=dropout)
 
-        self.multi_head_layer = nn.Linear(259,256)
+		self.multi_head_layer = nn.Linear(259,256)
 
-        self.linear_decoder_x = nn.Linear(259,256)
-        self.linear_x = nn.Linear(256,252)
-        self.goal_expand = nn.Linear(10,128)
+		self.linear_decoder_x = nn.Linear(259,256)
+		self.linear_x = nn.Linear(256,252)
+		self.goal_expand = nn.Linear(10,128)
 
-        self.context_conv = nn.Conv1d(in_channels=2, out_channels=1, kernel_size=3,padding=1)
-        self.context_linear = nn.Linear(15,7)
-        self.relu = nn.ReLU()
-        self.output_relu = nn.ReLU()
-        self.init_weights()
+		self.context_conv = nn.Conv1d(in_channels=2, out_channels=1, kernel_size=3,padding=1)
+		self.context_linear = nn.Linear(15,7)
+		self.relu = nn.ReLU()
+		self.output_relu = nn.ReLU()
+		self.init_weights()
 
-    def init_weights(self):
+	def init_weights(self):
 
-        self.linear_decoder_x.weight.data.normal_(0, 0.05)
-        self.context_linear.weight.data.normal_(0, 0.05)
-        self.context_conv.weight.data.normal_(0, 0.1)
-
-
-    def forward(self, x, y, goal_position,context,sort=False):       
-
-        output = torch.zeros([x.shape[2],252]).to(device)
-        softmax_out =torch.zeros([x.shape[2],252]).to(device)
-        latent_space_ =torch.zeros([x.shape[2],259]).to(device)
-        
-        encoded_trajectories_x = []
-        encoded_appended_trajectories_x = []
-        encoded_trajectories_y = []
-        x1 = torch.transpose(x, 0, 2)
-
-        if x.shape[2] <batch_size:
-            input_pos =torch.reshape(x1[:,:,0], (x.shape[2],3))
-        input_pos =torch.reshape(x1[:,:,0], (x.shape[2],3))
-
-        if goal_position[0,0].cpu()<800 and goal_position[0,0].cpu()> -10 and abs(goal_position[0,1].cpu())<300: #1
-            goal_position[0,0],goal_position[0,1] =0.0, 0.0
-
-        elif goal_position[0,0].cpu()<1500 and goal_position[0,0].cpu()> 1000 and abs(goal_position[0,1].cpu())<300:  #2,
-            goal_position[0,0],goal_position[0,1] =1450.0, 0.0
-
-        goal_vector = direction_detect(input_pos.cpu(), goal_position.cpu())
-
-        goal_vector= goal_vector.to(device)
-        goal_expanded =self.goal_expand(goal_vector)
-
-        x1=x1/1000.0
-        encoded_x = self.tcn_encoder_x(x1)
-
-        encoded_x =encoded_x[:,:,-1]
-        full_encoded_x = torch.cat((encoded_x,goal_expanded,goal_position/1000.0),1)
-        encoded_appended_trajectories_x.append(full_encoded_x)
-
-        final_input = torch.squeeze(torch.stack(encoded_appended_trajectories_x))
+		self.linear_decoder_x.weight.data.normal_(0, 0.05)
+		self.context_linear.weight.data.normal_(0, 0.05)
+		self.context_conv.weight.data.normal_(0, 0.1)
 
 
-        latent_space = final_input         
-        H_x = final_input
-        decoded1 = (self.linear_decoder_x(H_x))
-        decoded2 = (self.linear_x(decoded1))
+	def forward(self, x, y, goal_position,context,sort=False):       
 
-	multi_head = self.multi_head_layer(H_x)
+		output = torch.zeros([x.shape[2],252]).to(device)
+		softmax_out =torch.zeros([x.shape[2],252]).to(device)
+		latent_space_ =torch.zeros([x.shape[2],259]).to(device)
+		
+		encoded_trajectories_x = []
+		encoded_appended_trajectories_x = []
+		encoded_trajectories_y = []
+		x1 = torch.transpose(x, 0, 2)
 
-        output = torch.squeeze(decoded2,dim=0)
+		if x.shape[2] <batch_size:
+			input_pos =torch.reshape(x1[:,:,0], (x.shape[2],3))
+		input_pos =torch.reshape(x1[:,:,0], (x.shape[2],3))
 
-        if int(x.shape[2]) == 1:
-            softmax_out = F.softmax(decoded2, dim = 0)
+		if goal_position[0,0].cpu()<800 and goal_position[0,0].cpu()> -10 and abs(goal_position[0,1].cpu())<300: #1
+			goal_position[0,0],goal_position[0,1] =0.0, 0.0
 
-        else:
-            softmax_out = F.softmax(decoded2, dim = 1)
-        return softmax_out, output,latent_space_, multi_head    
+		elif goal_position[0,0].cpu()<1500 and goal_position[0,0].cpu()> 1000 and abs(goal_position[0,1].cpu())<300:  #2,
+			goal_position[0,0],goal_position[0,1] =1450.0, 0.0
+
+		goal_vector = direction_detect(input_pos.cpu(), goal_position.cpu())
+
+		goal_vector= goal_vector.to(device)
+		goal_expanded =self.goal_expand(goal_vector)
+
+		x1=x1/1000.0
+		encoded_x = self.tcn_encoder_x(x1)
+
+		encoded_x =encoded_x[:,:,-1]
+		full_encoded_x = torch.cat((encoded_x,goal_expanded,goal_position/1000.0),1)
+		encoded_appended_trajectories_x.append(full_encoded_x)
+
+		final_input = torch.squeeze(torch.stack(encoded_appended_trajectories_x))
+
+
+		latent_space = final_input         
+		H_x = final_input
+		decoded1 = (self.linear_decoder_x(H_x))
+		decoded2 = (self.linear_x(decoded1))
+
+		multi_head = self.multi_head_layer(H_x)
+
+		output = torch.squeeze(decoded2,dim=0)
+
+		if int(x.shape[2]) == 1:
+			softmax_out = F.softmax(decoded2, dim = 0)
+
+		else:
+			softmax_out = F.softmax(decoded2, dim = 1)
+		return softmax_out, output,latent_space_, multi_head    
